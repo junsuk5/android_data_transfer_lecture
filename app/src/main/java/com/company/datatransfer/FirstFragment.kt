acf001ab62eb76b887909bfc4c9efe4a6e13018f
Junsuk Oh <a811219@gmail.com>
2020-07-05 12:06:46 +0900

    ActivityResultContracts API를 사용한 방법

--- app/src/main/java/com/company/datatransfer/FirstFragment.kt
+++ app/src/main/java/com/company/datatransfer/FirstFragment.kt
@@ -1,34 +1,39 @@
 package com.company.datatransfer
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
-import androidx.core.os.bundleOf
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.setFragmentResult
-import androidx.fragment.app.setFragmentResultListener
-import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment(R.layout.fragment_first) {
 
+    val getContent = registerForActivityResult(
+        ActivityResultContracts.GetContent()
+    ) {
+        imageView.setImageURI(it)
+    }
+
+    val getStartActivityForResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { activityResult ->
+        activityResult.data?.let { intent ->
+            intent.extras?.let { bundle ->
+                Toast.makeText(requireContext(),"result : ${bundle.getString("data", "world")}",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setFragmentResultListener("requestKey") { resultKey, bundle ->
-            val data = bundle.getString("data", "")
-
-            Toast.makeText(requireContext(), data, Toast.LENGTH_SHORT).show()
-        }
-
         button.setOnClickListener {
-
-            setFragmentResult(
-                "requestKey",
-                bundleOf("data" to "hello")
-            )
-
-            findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+            Intent(requireContext(), ResultActivity::class.java).apply {
+                getStartActivityForResult.launch(this)
+            }
         }
     }
 }
